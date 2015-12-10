a07ef2310ffda0961bfd88679d0714a177b12363
Maxwell Dergosits <mdergosits@gmail.com>
2015-12-10 00:09:37 -0500

    cleaned up some stuff

--- gossip_test.go
+++ gossip_test.go
@@ -5,10 +5,6 @@ import "time"
 
 type TestContext int
 
-func (t *TestContext) RoundLength() time.Duration { return 1 }
-func (t *TestContext) SyncLength() time.Duration  { return 1 }
-func (t *TestContext) RoundSize() uint            { return 10 }
-
 func Test(t *testing.T) {
 
 }
